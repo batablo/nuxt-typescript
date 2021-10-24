f48cb8dd5d42d15aa3c26a62d1bfd282211abd0f
batablo <mutemath.us22@gmail.com>
2021-10-24 20:38:24 +0900

    add: MyTextArea

---
+++ pages/form/-MyTextArea.vue
@@ -0,0 +1,25 @@
+<template>
+  <textarea type="text" @input="ipnutHandler" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  props: {
+    value: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+  emits: ['input'],
+  setup(_, context) {
+    const ipnutHandler = (e: Event) => {
+      const target = e.target as HTMLInputElement;
+      context.emit('input', target.value);
+    };
+
+    return { ipnutHandler };
+  },
+});
+</script>
--- pages/form/index.vue
+++ pages/form/index.vue
@@ -3,13 +3,17 @@
     <div>
       {{ form }}
     </div>
-    <MyInput v-model="form.text" />
+    <div class="form-wrapper">
+      <MyInput v-model="form.text" class="form" />
+      <MyTextArea v-model="form.longText" class="form" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from '@nuxtjs/composition-api';
 import MyInput from './-MyInput.vue';
+import MyTextArea from './-MyTextArea.vue';
 
 interface State {
   text: string;
@@ -20,7 +24,7 @@ interface State {
 }
 
 export default defineComponent({
-  components: { MyInput },
+  components: { MyInput, MyTextArea },
   setup() {
     const form = reactive<State>({
       text: 'init text',
@@ -40,3 +44,14 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.form-wrapper {
+  display: flex;
+  flex-direction: column;
+}
+.form {
+  border: solid 1px;
+  margin: 8px 0;
+}
+</style>
