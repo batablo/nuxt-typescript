8811dd244b24a850d2796f8cf3d8389397840978
batablo <mutemath.us22@gmail.com>
2021-10-24 14:02:03 +0900

    add: composition API(ref, reactive)

---
+++ components/Sample.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="u in data.users" :key="u.id">{{ u.id }}{{ u.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent, reactive, ref } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      users: [
+        { id: 1, name: '山田太郎' },
+        { id: 2, name: '田中浩一' },
+        { id: 3, name: '鈴木花子' },
+      ],
+    });
+
+    const title = ref('タイトル');
+
+    setTimeout(() => {
+      data.users.push({ id: 4, name: 'ジョンソン' });
+      title.value = 'タイトルが変更できる';
+    }, 5000);
+
+    return { data, title };
+  },
+});
+</script>
+
+<style></style>
