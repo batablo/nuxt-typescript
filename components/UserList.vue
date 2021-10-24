e94c0fd9b2ba0b8e07e401572f01cf626650fe7f
batablo <mutemath.us22@gmail.com>
2021-10-24 15:18:56 +0900

    add: composition API(props)

---
+++ components/UserList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>ユーザー件数: {{ userNum }}</p>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }}: {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { defineComponent, computed } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props) {
+    const userNum = computed(() => props.users.length);
+
+    return { userNum };
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
-<template><Sample /></template>
+<template>
+  <div>
+    <UserList :users="data.users" />
+  </div>
+</template>
 
 <script>
-import Sample from '@/components/Sample';
+import { defineComponent, reactive } from '@nuxtjs/composition-api';
 
-export default {
-  components: {
-    Sample,
+export default defineComponent({
+  setup() {
+    const data = reactive({
+      users: [
+        { id: 1, name: '加藤かな' },
+        { id: 2, name: '田中紘一' },
+        { id: 3, name: '山田太郎' },
+      ],
+    });
+
+    setTimeout(() => {
+      data.users.push({ id: 4, name: '木下武' });
+    }, 3000);
+
+    return { data };
   },
-};
+});
 </script>
