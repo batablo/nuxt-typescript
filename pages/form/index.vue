33429151a39c3557d3761959449bed21c247d9af
batablo <mutemath.us22@gmail.com>
2021-10-24 19:10:57 +0900

    add: MyInput

---
+++ pages/form/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      {{ form }}
+    </div>
+    <MyInput v-model="form.text" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from '@nuxtjs/composition-api';
+import MyInput from './-MyInput.vue';
+
+interface State {
+  text: string;
+  longText: string;
+  checked: boolean;
+  picked: string;
+  selected: string;
+}
+
+export default defineComponent({
+  components: { MyInput },
+  setup() {
+    const form = reactive<State>({
+      text: 'init text',
+      longText: 'init long text',
+      checked: false,
+      picked: 'two',
+      selected: 'b',
+    });
+
+    const options = [
+      { label: 'A', value: 'a' },
+      { label: 'B', value: 'b' },
+      { label: 'C', value: 'c' },
+    ];
+
+    return { form, options };
+  },
+});
+</script>
