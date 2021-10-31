248a6e3d3cb095e560e555d167f16087c8100bc1
batablo <mutemath.us22@gmail.com>
2021-10-31 17:12:05 +0900

    add: デプロイ環境構築

---
+++ pages/calender/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-sheet height="500">
+    <h2>カレンダー</h2>
+    <Calender />
+  </v-sheet>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api';
+import Calender from './-Calender.vue';
+
+export default defineComponent({
+  components: { Calender },
+  setup() {},
+});
+</script>
