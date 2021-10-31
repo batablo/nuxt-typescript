df3064af914c60c932243c89546dc8f049ff6d1e
batablo <mutemath.us22@gmail.com>
2021-10-31 12:22:20 +0900

    add: firebase v9

---
+++ plugins/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIysuFnDXqa7yzGarKgRoBrQVjEP41X8Y',
+  authDomain: 'batacook-a9a39.firebaseapp.com',
+  projectId: 'batacook-a9a39',
+  storageBucket: 'batacook-a9a39.appspot.com',
+  messagingSenderId: '139842308418',
+  appId: '1:139842308418:web:d15ef97311eac723ed20db',
+  measurementId: 'G-JVSLPYMGLP',
+};
+
+const apps = getApps();
+
+if (!apps.length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = apps[0];
+}
+
+const db = getFirestore(firebaseApp, {});
+export { db };
